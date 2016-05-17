e1859ef034c1083e531adcc61a33759e559fa37f
vh4x <templar996@gmail.com>
2016-05-17 10:57:12 +0300

    Fixed incorrect indentation level in comment

--- zipkin_client.py
+++ zipkin_client.py
@@ -28,7 +28,7 @@ class ZipkinClient(ScribeClient):
         return create_annotation(timestamp, value, host)
 
     def create_binary_annotation(self, service_name, ipv4, port, key, val):
-         """
+        """
         Creates a binary annotation thrift object
 
         :param service_name: name that identifies the service of the endpoint
@@ -37,9 +37,9 @@ class ZipkinClient(ScribeClient):
         :param host: zipkin endpoint object
 
         :returns: zipkin binary annotation object
-        """        
+        """
         host = create_endpoint(ipv4=ipv4, port=port, service_name=service_name)
-        return create_binary_annotation(key, val, zipkin_core.AnnotationType.STRING, host)   
+        return create_binary_annotation(key, val, zipkin_core.AnnotationType.STRING, host)
 
     def scribe_record(self, message):
         category = 'zipkin'
